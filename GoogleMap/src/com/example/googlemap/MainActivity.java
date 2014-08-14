7e1ef1d9fcbba50ea7040db3343b50d4710db1e4
sanam dongol <sanam.dongol@gmail.com>
2014-08-14 14:51:46 +0545

    Google Map with Marker

---
+++ GoogleMap/src/com/example/googlemap/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.googlemap;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends ActionBarActivity {
+	static final LatLng myLoc = new LatLng(27.6787855,85.32227680000005);
+	private GoogleMap googleMap;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		try {
+			// Loading map
+			initilizeMap();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * function to load map. If map is not created it will create it for you
+	 * */
+	private void initilizeMap() {
+		if (googleMap == null) {
+			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.map)).getMap();
+
+			// check if map is created successfully or not
+
+			Marker TP = googleMap.addMarker(new MarkerOptions().position(myLoc)
+					.title("I am here now"));
+
+		    // Move the camera instantly to hamburg with a zoom of 15.
+		   googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLoc, 5));
+
+		    // Zoom in, animating the camera.
+		   googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+			if (googleMap == null) {
+				Toast.makeText(getApplicationContext(),
+						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
+						.show();
+			}
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initilizeMap();
+	}
+}
